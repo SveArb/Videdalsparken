1df4205d0409fdddc64a9a8ed8318102955ca680
SveArb <info@svearb.se>
2025-02-26 14:01:24 +0100

    Add files via upload

---
+++ layers/layers.js
@@ -0,0 +1,40 @@
+var wms_layers = [];
+
+
+        var lyr_GoogleSatellit_0 = new ol.layer.Tile({
+            'title': 'Google Satellit',
+            'opacity': 1.000000,
+            
+            
+            source: new ol.source.XYZ({
+            attributions: ' ',
+                url: 'http://mt0.google.com/vt/lyrs=s&hl=en&x={x}&y={y}&z={z}'
+            })
+        });
+var format_survey_1 = new ol.format.GeoJSON();
+var features_survey_1 = format_survey_1.readFeatures(json_survey_1, 
+            {dataProjection: 'EPSG:4326', featureProjection: 'EPSG:3857'});
+var jsonSource_survey_1 = new ol.source.Vector({
+    attributions: ' ',
+});
+jsonSource_survey_1.addFeatures(features_survey_1);
+var lyr_survey_1 = new ol.layer.Vector({
+                declutter: false,
+                source:jsonSource_survey_1, 
+                style: style_survey_1,
+                popuplayertitle: 'survey',
+                interactive: true,
+    title: 'survey<br />\
+    <img src="styles/legend/survey_1_0.png" /> <br />\
+    <img src="styles/legend/survey_1_1.png" /> <br />\
+    <img src="styles/legend/survey_1_2.png" /> <br />\
+    <img src="styles/legend/survey_1_3.png" /> <br />' });
+
+lyr_GoogleSatellit_0.setVisible(true);lyr_survey_1.setVisible(true);
+var layersList = [lyr_GoogleSatellit_0,lyr_survey_1];
+lyr_survey_1.set('fieldAliases', {'fid': 'fid', 'Nr': 'Nr', 'notes': 'Art', 'unique-id': 'unique-id', 'höjd': 'Höjd', 'dbh': 'Stamdiameter', 'vitalitet': 'Vitalitet', 'rotskador': 'Rotskador', 'stamskador': 'Stamskador', 'kronskador': 'Kronskador', 'svamp': 'Svamp', 'risk': 'Risk', 'skadegörare': 'Skadegörare', 'biologiskt_värde': 'Biologiskt värde', 'bevarandevärde': 'Bevarandevärde', 'skyddsvärt_träd': 'Skyddsvärt Träd', 'åtgärd': 'Åtgärd', 'tidsåtgång': 'Tidsåtgång', });
+lyr_survey_1.set('fieldImages', {'fid': 'Hidden', 'Nr': 'TextEdit', 'notes': 'TextEdit', 'unique-id': 'Hidden', 'höjd': 'Range', 'dbh': 'Range', 'vitalitet': 'ValueMap', 'rotskador': 'ValueMap', 'stamskador': 'ValueMap', 'kronskador': 'ValueMap', 'svamp': 'ValueMap', 'risk': 'ValueMap', 'skadegörare': 'TextEdit', 'biologiskt_värde': 'ValueMap', 'bevarandevärde': 'Hidden', 'skyddsvärt_träd': 'ValueMap', 'åtgärd': 'TextEdit', 'tidsåtgång': 'Range', });
+lyr_survey_1.set('fieldLabels', {'Nr': 'inline label - always visible', 'notes': 'hidden field', 'höjd': 'hidden field', 'dbh': 'hidden field', 'vitalitet': 'hidden field', 'rotskador': 'hidden field', 'stamskador': 'hidden field', 'kronskador': 'hidden field', 'svamp': 'hidden field', 'risk': 'hidden field', 'skadegörare': 'hidden field', 'biologiskt_värde': 'hidden field', 'skyddsvärt_träd': 'hidden field', 'åtgärd': 'inline label - visible with data', 'tidsåtgång': 'inline label - visible with data', });
+lyr_survey_1.on('precompose', function(evt) {
+    evt.context.globalCompositeOperation = 'normal';
+});
---
+++ styles/survey_1_style.js
@@ -0,0 +1,68 @@
+var size = 0;
+var placement = 'point';
+function categories_survey_1(feature, value, size, resolution, labelText,
+                       labelFont, labelFill, bufferColor, bufferWidth,
+                       placement) {
+                var valueStr = (value !== null && value !== undefined) ? value.toString() : 'default';
+                switch(valueStr) {case '1':
+                    return [ new ol.style.Style({
+        image: new ol.style.Circle({radius: 8.0 + size,
+            displacement: [0, 0], stroke: new ol.style.Stroke({color: 'rgba(35,35,35,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0.0}), fill: new ol.style.Fill({color: 'rgba(31,255,1,1.0)'})}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;
+case '2':
+                    return [ new ol.style.Style({
+        image: new ol.style.Circle({radius: 8.0 + size,
+            displacement: [0, 0], stroke: new ol.style.Stroke({color: 'rgba(35,35,35,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0.0}), fill: new ol.style.Fill({color: 'rgba(104,194,216,1.0)'})}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;
+case '3':
+                    return [ new ol.style.Style({
+        image: new ol.style.Circle({radius: 8.0 + size,
+            displacement: [0, 0], stroke: new ol.style.Stroke({color: 'rgba(35,35,35,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0.0}), fill: new ol.style.Fill({color: 'rgba(232,255,61,1.0)'})}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;
+case '4':
+                    return [ new ol.style.Style({
+        image: new ol.style.Circle({radius: 8.0 + size,
+            displacement: [0, 0], stroke: new ol.style.Stroke({color: 'rgba(35,35,35,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0.0}), fill: new ol.style.Fill({color: 'rgba(255,0,4,1.0)'})}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;}};
+
+var style_survey_1 = function(feature, resolution){
+    var context = {
+        feature: feature,
+        variables: {}
+    };
+    
+    var labelText = ""; var value = feature.get("risk");
+    var labelFont = "6.5px \'Open Sans\', sans-serif";
+    var labelFill = "#323232";
+    var bufferColor = "";
+    var bufferWidth = 0;
+    var textAlign = "center";
+    var offsetX = 0;
+    var offsetY = -15;
+    var placement = 'point';
+    if (feature.get("Nr") !== null) {
+        labelText = String(feature.get("Nr"));
+    }
+    
+    var style = categories_survey_1(feature, value, size, resolution, labelText,
+                            labelFont, labelFill, bufferColor,
+                            bufferWidth, placement);
+
+    return style;
+};
